696401fb5a5dd8f23a1afe7cc487991d0ce2033a
ewonguoft <eltong.wong@mail.utoronto.ca>
2018-10-11 14:15:44 -0400

    init lab2

---
+++ client.c
@@ -0,0 +1,82 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define HOST "localhost"
+#define PORT 8765
+
+/* use these strings to tell the marker what is happening */
+#define FMT_CONNECT_ERR "ECE568-CLIENT: SSL connect error\n"
+#define FMT_SERVER_INFO "ECE568-CLIENT: %s %s %s\n"
+#define FMT_OUTPUT "ECE568-CLIENT: %s %s\n"
+#define FMT_CN_MISMATCH "ECE568-CLIENT: Server Common Name doesn't match\n"
+#define FMT_EMAIL_MISMATCH "ECE568-CLIENT: Server Email doesn't match\n"
+#define FMT_NO_VERIFY "ECE568-CLIENT: Certificate does not verify\n"
+#define FMT_INCORRECT_CLOSE "ECE568-CLIENT: Premature close\n"
+
+int main(int argc, char **argv)
+{
+  int len, sock, port=PORT;
+  char *host=HOST;
+  struct sockaddr_in addr;
+  struct hostent *host_entry;
+  char buf[256];
+  char *secret = "What's the question?";
+  
+  /*Parse command line arguments*/
+  
+  switch(argc){
+    case 1:
+      break;
+    case 3:
+      host = argv[1];
+      port=atoi(argv[2]);
+      if (port<1||port>65535){
+	fprintf(stderr,"invalid port number");
+	exit(0);
+      }
+      break;
+    default:
+      printf("Usage: %s server port\n", argv[0]);
+      exit(0);
+  }
+  
+  /*get ip address of the host*/
+  
+  host_entry = gethostbyname(host);
+  
+  if (!host_entry){
+    fprintf(stderr,"Couldn't resolve host");
+    exit(0);
+  }
+
+  memset(&addr,0,sizeof(addr));
+  addr.sin_addr=*(struct in_addr *) host_entry->h_addr_list[0];
+  addr.sin_family=AF_INET;
+  addr.sin_port=htons(port);
+  
+  printf("Connecting to %s(%s):%d\n", host, inet_ntoa(addr.sin_addr),port);
+  
+  /*open socket*/
+  
+  if((sock=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))<0)
+    perror("socket");
+  if(connect(sock,(struct sockaddr *)&addr, sizeof(addr))<0)
+    perror("connect");
+  
+  send(sock, secret, strlen(secret),0);
+  len = recv(sock, &buf, 255, 0);
+  buf[len]='\0';
+  
+  /* this is how you output something for the marker to pick up */
+  printf(FMT_OUTPUT, secret, buf);
+  
+  close(sock);
+  return 1;
+}
---
+++ server.c
@@ -0,0 +1,100 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8765
+
+/* use these strings to tell the marker what is happening */
+#define FMT_ACCEPT_ERR "ECE568-SERVER: SSL accept error\n"
+#define FMT_CLIENT_INFO "ECE568-SERVER: %s %s\n"
+#define FMT_OUTPUT "ECE568-SERVER: %s %s\n"
+#define FMT_INCOMPLETE_CLOSE "ECE568-SERVER: Incomplete shutdown\n"
+
+int main(int argc, char **argv)
+{
+  int s, sock, port=PORT;
+  struct sockaddr_in sin;
+  int val=1;
+  pid_t pid;
+  
+  /*Parse command line arguments*/
+  
+  switch(argc){
+    case 1:
+      break;
+    case 2:
+      port=atoi(argv[1]);
+      if (port<1||port>65535){
+	fprintf(stderr,"invalid port number");
+	exit(0);
+      }
+      break;
+    default:
+      printf("Usage: %s port\n", argv[0]);
+      exit(0);
+  }
+
+  if((sock=socket(AF_INET,SOCK_STREAM,0))<0){
+    perror("socket");
+    close(sock);
+    exit(0);
+  }
+  
+  memset(&sin,0,sizeof(sin));
+  sin.sin_addr.s_addr=INADDR_ANY;
+  sin.sin_family=AF_INET;
+  sin.sin_port=htons(port);
+  
+  setsockopt(sock,SOL_SOCKET,SO_REUSEADDR, &val,sizeof(val));
+    
+  if(bind(sock,(struct sockaddr *)&sin, sizeof(sin))<0){
+    perror("bind");
+    close(sock);
+    exit (0);
+  }
+  
+  if(listen(sock,5)<0){
+    perror("listen");
+    close(sock);
+    exit (0);
+  } 
+  
+  while(1){
+    
+    if((s=accept(sock, NULL, 0))<0){
+      perror("accept");
+      close(sock);
+      close(s);
+      exit (0);
+    }
+    
+    /*fork a child to handle the connection*/
+    
+    if((pid=fork())){
+      close(s);
+    }
+    else {
+      /*Child code*/
+      int len;
+      char buf[256];
+      char *answer = "42";
+
+      len = recv(s, &buf, 255, 0);
+      buf[len]= '\0';
+      printf(FMT_OUTPUT, buf, answer);
+      send(s, answer, strlen(answer), 0);
+      close(sock);
+      close(s);
+      return 0;
+    }
+  }
+  
+  close(sock);
+  return 1;
+}
